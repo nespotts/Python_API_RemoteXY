ad6a4957b1375962c51f3c148d936a456e1ba3e0
Nathan Spotts <nathan_spotts@hotmail.com>
2024-07-24 23:24:12 +0000

    check in

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import time
 import json
 app = FastAPI()
 
@@ -36,6 +37,11 @@ def get_value(key: str):
   return variables[key]
 
 
+@app.get("/get_all")
+def get_all():
+  return variables
+
+
 @app.get("/set_value/{key}/{value}")
 def set_value(key: str, value):
   if key not in variables:
@@ -45,18 +51,13 @@ def set_value(key: str, value):
   return variables[key]
 
 
-@app.get("/get_all")
-def get_all():
-  return variables
-
-
 @app.get("/set_value")
 def set_value2(key: str, value):
   if key not in variables:
     return {"error": "Key not found"}
   variables[key] = value
   cache_variables(variables)
-  return variables[key]
+  # return variables[key]
 
 
 @app.get("/set_bms_params/{bms_id}/{voltage}/{current}/{state_of_charge}/{state_of_charge_1}/{discharge_status}/{charge_status}/{battery_temperature}/{bms_temperature}/{balance_capacity}/{rate_capacity}/{cell1_voltage}/{cell2_voltage}/{cell3_voltage}/{cell4_voltage}/{cell1_balance_enabled}/{cell2_balance_enabled}/{cell3_balance_enabled}/{cell4_balance_enabled}/{cycle_count}/{fault_count}/{max_cell_delta}")
@@ -66,7 +67,7 @@ def set_bms_parameters(bms_id, voltage, current, state_of_charge, state_of_charg
   }
   variables.update(parameters)
   cache_variables(variables)
-  return variables
+  # return variables
 
 @app.get("/set_battery_params/{voltage}/{current}/{power}/{state_of_charge}/{state_of_charge_percent}")
 def set_battery_params(voltage, current, power, state_of_charge, state_of_charge_percent):
@@ -76,7 +77,7 @@ def set_battery_params(voltage, current, power, state_of_charge, state_of_charge
   variables["battery_state_of_charge"] = state_of_charge
   variables["battery_state_of_charge_percent"] = state_of_charge_percent
   cache_variables(variables)
-  return variables
+  # return variables
 
 
 @app.get("/set_solar_params/{solar_id}/{voltage}/{current}/{power}/{battery_voltage}/{battery_current}/{state_of_charge}/{controller_temp}")
@@ -89,7 +90,7 @@ def set_solar_params(solar_id, voltage, current, power, battery_voltage, battery
   variables[f"solar{solar_id}_state_of_charge"] = state_of_charge
   variables[f"solar{solar_id}_controller_temp"] = controller_temp
   cache_variables(variables)
-  return variables
+  # return variables
 
 
 @app.get("/set_temps/{inside_temperature}/{outside_temperature}/{electrical_cabinet_temperature}")
@@ -98,6 +99,4 @@ def set_temps(inside_temperature, outside_temperature, electrical_cabinet_temper
   variables["outside_temperature"] = outside_temperature
   variables["electrical_cabinet_temperature"] = electrical_cabinet_temperature
   cache_variables(variables)
-  return variables
-
-  
+  # return variables
