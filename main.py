5a51ab7edc22f12cdcf266363286eb3f23ebbe7b
Nathan Spotts <nathan_spotts@hotmail.com>
2024-07-24 01:20:24 -0400

    check in

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import json
 app = FastAPI()
 
+
 variables = {
   "connect_flag": 0,
 }
@@ -58,13 +59,37 @@ def set_value2(key: str, value):
   return variables[key]
 
 
-@app.get("/set_bms_params/{bms}/{voltage}/{current}/{state_of_charge}/{state_of_charge1}/{discharge_status}/{charge_status}/{battery_temp}/{bms_temp}/{balance_capacity}/{rate_capacity}/{cell1_voltage}/{cell2_voltage}/{cell3_voltage}/{cell4_voltage}/{cell1_bal_en}/{cell2_bal_en}/{cell3_bal_en}/{cell4_bal_en}/{cycle_count}/{fault_count}/{max_cell_delta}")
-def set_bms_params(bms, voltage, current, state_of_charge, state_of_charge1, discharge_status, charge_status, battery_temp, bms_temp, balance_capacity, rate_capacity, cell1_voltage, cell2_voltage, cell3_voltage, cell4_voltage, cell1_bal_en, cell2_bal_en, cell3_bal_en, cell4_bal_en, cycle_count, fault_count, max_cell_delta):
-  parameters = locals()
-  parameters.pop("bms")
-  for key,value in parameters.items():
-    key = f"bms{bms}_{key}"
-    # print(key, value)
-    variables[key] = value
+@app.get("/set_bms_params/{bms_id}/{voltage}/{current}/{state_of_charge}/{state_of_charge_1}/{discharge_status}/{charge_status}/{battery_temperature}/{bms_temperature}/{balance_capacity}/{rate_capacity}/{cell1_voltage}/{cell2_voltage}/{cell3_voltage}/{cell4_voltage}/{cell1_balance_enabled}/{cell2_balance_enabled}/{cell3_balance_enabled}/{cell4_balance_enabled}/{cycle_count}/{fault_count}/{max_cell_delta}")
+def set_bms_parameters(bms_id, voltage, current, state_of_charge, state_of_charge_1, discharge_status, charge_status, battery_temperature, bms_temperature, balance_capacity, rate_capacity, cell1_voltage, cell2_voltage, cell3_voltage, cell4_voltage, cell1_balance_enabled, cell2_balance_enabled, cell3_balance_enabled, cell4_balance_enabled, cycle_count, fault_count, max_cell_delta):
+  parameters = {
+      f"bms{bms_id}_{key}": value for key, value in locals().items() if key != "bms_id"
+  }
+  variables.update(parameters)
   cache_variables(variables)
   return variables
+
+@app.get("/set_battery_params/{voltage}/{current}/{power}/{state_of_charge}/{state_of_charge_percent}")
+def set_battery_params(voltage, current, power, state_of_charge, state_of_charge_percent):
+  variables["battery_voltage"] = voltage
+  variables["battery_current"] = current
+  variables["battery_power"] = power
+  variables["battery_state_of_charge"] = state_of_charge
+  variables["battery_state_of_charge_percent"] = state_of_charge_percent
+  cache_variables(variables)
+  return variables
+
+
+# function solar_controller
+@app.get("/set_solar_params/{solar_id}/{voltage}/{current}/{power}/{battery_voltage}/{battery_current}/{state_of_charge}/{controller_temp}")
+def set_solar_params(solar_id, voltage, current, power, battery_voltage, battery_current, state_of_charge, controller_temp):
+  variables[f"solar{solar_id}_voltage"] = voltage
+  variables[f"solar{solar_id}_current"] = current
+  variables[f"solar{solar_id}_power"] = power
+  variables[f"solar{solar_id}_battery_voltage"] = battery_voltage
+  variables[f"solar{solar_id}_battery_current"] = battery_current
+  variables[f"solar{solar_id}_state_of_charge"] = state_of_charge
+  variables[f"solar{solar_id}_controller_temp"] = controller_temp
+  cache_variables(variables)
+  return variables
+
+  
